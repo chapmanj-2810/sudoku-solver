6f4ab202ac48ceb3d1dbf6b079812ef4b4df3a4a
jchapman-2810 <jameschapman8032@hotmail.com>
2021-03-25 21:03:48 +0000

    Increased code efficiency by a factor of 2. The next job will be to swap the conditions into the loop that adds the values into the puzzle, checking if the individual value breaks a condition. This method would save computation by not constructing puzzles from numbers that voilate the conditions before testing them.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,8 +1,9 @@
 import numpy as np
-from itertools import permutations, product
+from itertools import permutations
+import matplotlib.pyplot as plt
 
-test_case = np.genfromtxt('hard_sudoku.txt', delimiter=',', dtype=int) 
-test_solution = np.genfromtxt('hard_sudoku_sol.txt', delimiter=',', dtype=int) 
+test_case = np.genfromtxt('sudoku_test.csv', delimiter=',', dtype=int) 
+test_solution = np.genfromtxt('sudoku_sols.csv', delimiter=',', dtype=int) 
 
 def iterFlatten(root):
 	'''iterFlatten takes a nested list input and yields a flattened list where every element contained within the nested list
@@ -16,24 +17,20 @@ def iterFlatten(root):
 
 def sudoku_solver(puzzle):
 	values, counts = np.unique(puzzle, return_counts=True) #The different values found within the grid and their frequencies.
-	missing_numbers = [[value]*(9-count) for value,count in zip(values,counts) if value != 0 and count != 0]
+	missing_numbers = [[value]*(9-count) for value,count in zip(values,counts) if value != 0]
 	missing_numbers_flatlist = list(iterFlatten(missing_numbers))
 
 	unique_combinations = set(permutations(missing_numbers_flatlist, len(missing_numbers_flatlist)))
 
 	x_ind, y_ind = np.where(puzzle==0)
-	indices = np.column_stack([x_ind, y_ind])
 
 	for combo in unique_combinations:
 
-		for val,ind in zip(combo, indices):
-			puzzle[ind[0], ind[1]] = val		
+		for val, indx, indy in zip(combo, x_ind, y_ind):
+			puzzle[indx, indy] = val		
 
-		if len(set(puzzle[:,0]))==9 & len(set(puzzle[:,1]))==9 & len(set(puzzle[:,2]))==9 & len(set(puzzle[:,3]))==9 & len(set(puzzle[:,4]))==9 & len(set(puzzle[:,5]))==9 & len(set(puzzle[:,6]))==9 & len(set(puzzle[:,7]))==9 & len(set(puzzle[:,8]))==9 & len(set(puzzle[0,:]))==9 & len(set(puzzle[1,:]))==9 & len(set(puzzle[2,:]))==9  & len(set(puzzle[3,:]))==9 & len(set(puzzle[4,:]))==9 & len(set(puzzle[5,:]))==9 & len(set(puzzle[6,:]))==9 & len(set(puzzle[7,:]))==9  & len(set(puzzle[8,:]))==9:
-			if len(set(puzzle[:3,:3].flatten()))==9 & len(set(puzzle[:3,3:6].flatten()))==9 & len(set(puzzle[:3,6:].flatten()))==9 & len(set(puzzle[3:6,:3].flatten()))==9 & len(set(puzzle[3:6,3:6].flatten()))==9 & len(set(puzzle[3:6,6:].flatten()))==9 & len(set(puzzle[6:,:3].flatten()))==9 & len(set(puzzle[6:,3:6].flatten()))==9 & len(set(puzzle[6:,6:].flatten()))==9:
-				return(puzzle)
-			else:
-				continue
+		if (len(set(puzzle[:,0]))==9 and len(set(puzzle[:,1]))==9 and len(set(puzzle[:,2]))==9 and len(set(puzzle[:,3]))==9 and len(set(puzzle[:,4]))==9 and len(set(puzzle[:,5]))==9 and len(set(puzzle[:,6]))==9 and len(set(puzzle[:,7]))==9 and len(set(puzzle[:,8]))==9 and len(set(puzzle[0,:]))==9 and len(set(puzzle[1,:]))==9 and len(set(puzzle[2,:]))==9  and len(set(puzzle[3,:]))==9 and len(set(puzzle[4,:]))==9 and len(set(puzzle[5,:]))==9 and len(set(puzzle[6,:]))==9 and len(set(puzzle[7,:]))==9  and len(set(puzzle[8,:]))==9 and len(set(puzzle[:3,:3].flatten()))==9 and len(set(puzzle[:3,3:6].flatten()))==9 and len(set(puzzle[:3,6:].flatten()))==9 and len(set(puzzle[3:6,:3].flatten()))==9 and len(set(puzzle[3:6,3:6].flatten()))==9 and len(set(puzzle[3:6,6:].flatten()))==9 and len(set(puzzle[6:,:3].flatten()))==9 and len(set(puzzle[6:,3:6].flatten()))==9 and len(set(puzzle[6:,6:].flatten()))==9):
+			return(puzzle)
 		else:
 			continue
 				
